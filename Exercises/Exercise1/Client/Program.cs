0d5efc6ac5576b890b76632f8d49a8f11e6da3ca
Roppin.L <robin.ljungqvist@gmail.com>
2017-04-25 15:29:19 +0200

    Exercise 1 Service and Client

---
+++ Exercises/Exercise1/Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using Client.HelloWorldServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new HelloWorldClient("BasicHttp");
+            Console.WriteLine("Enter your first name.");
+            var first = Console.ReadLine();
+            Console.WriteLine("Enter your last name.");
+            var last = Console.ReadLine();
+            var NewName = new Name() { FirstName = first, LastName = last };
+            Console.WriteLine("Loading...");
+            var greeting = service.Hello(NewName);
+            Console.Clear();
+            Console.WriteLine(greeting);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercises/Exercise1/HelloWorldService/HelloWorldService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldService
+{
+    class HelloWorld: IHelloWorld
+    {
+        public string Hello(Name name)
+        {
+            return $"Hello, {name.FirstName} {name.LastName}!";
+        }
+    }
+    
+    [DataContract]
+    public class Name
+    {
+        [DataMember]
+        public string FirstName { get; set; }
+        [DataMember]
+        public string LastName { get; set; }
+
+    }
+    [ServiceContract]
+    public interface IHelloWorld
+    {
+        [OperationContract]
+        string Hello(Name name);
+    }
+}
