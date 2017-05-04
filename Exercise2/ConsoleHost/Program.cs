72f5df2cb486dea24e4bf97733abfd04da482dd5
Roppin.L <robin.ljungqvist@gmail.com>
2017-05-04 08:47:18 +0200

    Exercise 2

---
+++ Exercise2/ConsoleHost/Program.cs
@@ -0,0 +1,77 @@
+﻿using EvalServiceLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHost
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var host = new ServiceHost(typeof(EvalService));
+            host.AddServiceEndpoint(typeof(IEvalService), new BasicHttpBinding(), "http://localhost:8080/Evals/basic");
+            host.AddServiceEndpoint(typeof(IEvalService), new WSHttpBinding(), "http://localhost:8080/Evals/ws");
+            host.AddServiceEndpoint(typeof(IEvalService), new NetTcpBinding(), "net.tcp://localhost:8081/Evals/");
+
+
+
+            
+
+            try
+            {
+                host.Open();
+                PrintServiceInfo(host);
+                var evalService = new EvalService();
+                var eval = new Eval()
+                {
+                    Submitter = "Chuck Norris",
+                    TimeSent = DateTime.Now,
+                    Comment = "It Works!"
+                };
+                evalService.SubmitEval(eval);
+                var evalList = new List<Eval>();
+                evalList = evalService.GetEvals();
+                if (evalList != null)
+                {
+                    foreach (var e in evalList)
+                    {
+                        Console.WriteLine("Submitted Evaluations");
+                        Console.WriteLine("---------------------------------------------");
+                        Console.WriteLine($"Submitter: {e.Submitter} Time: {e.TimeSent}");
+                        Console.WriteLine($"Comment: {e.Comment}");
+                        Console.WriteLine("---------------------------------------------");
+
+
+                    }
+                }
+                host.Close();
+
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+                host.Abort();
+            }
+            Console.ReadLine();
+
+        }
+
+        private static void PrintServiceInfo(ServiceHost host)
+        {
+            Console.WriteLine($"Service Info: {host.Description.Name} {host.State.ToString()}!");
+            foreach (var endpoint in host.Description.Endpoints)
+            {
+                
+                Console.WriteLine($"{endpoint.Name}: {endpoint.Address}");
+                
+            }
+            
+
+        }
+    }
+}
---
+++ Exercise2/EvalService/EvalService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvalServiceLibrary
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class EvalService: IEvalService
+    {
+        List<Eval> Evals = new List<Eval>();
+
+        public List<Eval> GetEvals()
+        {
+            return Evals;
+        }
+
+        public void SubmitEval(Eval eval)
+        {
+            Evals.Add(eval);
+        }
+    }
+    [DataContract]
+    public class Eval
+    {
+        [DataMember]
+        public string Submitter { get; set; }
+        [DataMember]
+        public DateTime TimeSent { get; set; }
+        [DataMember]
+        public string Comment { get; set; }
+
+    }
+    [ServiceContract]
+    public interface IEvalService
+    {
+        [OperationContract]
+        void SubmitEval(Eval eval);
+        [OperationContract]
+        List<Eval> GetEvals();
+    }
+}
